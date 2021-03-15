b52e809b512c9ed770d5d50edd1b7cd4a501e2fd
ImranFX <80687882+ImranFX@users.noreply.github.com>
2021-03-15 18:44:19 +0600

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    console.log("Will the service worker register?");
-    navigator.serviceWorker.register('https://raw.githubusercontent.com/ImranFX/TF-PWA/main/service-worker.js')
-      .then(function(reg){
-        console.log("Yes, it did.");
-     }).catch(function(err) {
-        console.log("No it didn't. This happened:", err)
-    });
- }
