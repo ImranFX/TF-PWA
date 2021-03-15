9af0d69f4bf6d8364e5b1767d2bae41bb23a1d18
ImranFX <80687882+ImranFX@users.noreply.github.com>
2021-03-15 18:25:35 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     console.log("Will the service worker register?");
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('https://rawcdn.githack.com/ImranFX/TF-PWA/8a102e996669a6cdac4be540e37ce1e6bb5b70fa/service-worker.js')
       .then(function(reg){
         console.log("Yes, it did.");
      }).catch(function(err) {
